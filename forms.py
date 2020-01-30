084996620fc776c516a2888424e8e4832f86c40f
Matt Marum <mmarum@gmail.com>
2020-01-30 10:59:47 -0600

    added js date-picker

--- forms.py
+++ forms.py
@@ -7,14 +7,15 @@ from datetime import datetime
 #default_date_time = "2020-02-01-10-00"
 
 class EventsForm(Form):
-    #date_time = DateTimeField('Date Time', description='DateTime', default=default_date_time, format='%Y-%m-%d-%H-%M')
-    eid = HiddenField('eid', description='eid')
-    date_time = StringField('Date Time', description='DateTime')
-    duration = StringField('Duration', description='Duration')
-    title = StringField('Title', description='Title')
-    price = StringField('Price', description='Price')
-    location = StringField('Location', description='Location')
-    image = StringField('Image', description='Image')
-    description = TextAreaField('Description', description='Description')
-    submit = SubmitField('Submit', description='Submit')
+    #date_time = DateTimeField("Date Time", description="DateTime", default=default_date_time, format="%Y-%m-%d-%H-%M")
+    eid = HiddenField("eid", description="eid")
+    date = StringField("Date", description="Date")
+    time = StringField("Time", description="Time")
+    title = StringField("Title", description="Title")
+    duration = StringField("Duration (Hours)", description="Duration")
+    price = StringField("Price", description="Price")
+    location = StringField("Location", description="Location")
+    image = StringField("Image", description="Image")
+    description = TextAreaField("Description", description="Description")
+    submit = SubmitField("Submit", description="Submit")
 
