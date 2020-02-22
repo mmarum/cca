9daf326589e1f900c2bc073201932c514ecae46c
Matt Marum <mmarum@gmail.com>
2020-02-22 11:38:38 -0600

    fixed up contact form

---
+++ contact.py
@@ -0,0 +1,13 @@
+import os
+import sys
+sys.path.insert(0, os.path.dirname(__file__))
+def contact(environ, start_response):
+    start_response('200 OK', [('Content-Type', 'text/html; charset=utf-8')])
+    if environ['REQUEST_METHOD'] == "POST" and environ['PATH_INFO'] == "/submit" and environ['HTTP_REFERER'].endswith('about-contact.html'):
+        length = int(environ.get('CONTENT_LENGTH', '0'))
+        post_input = environ['wsgi.input'].read(length).decode('UTF-8')
+        with open("contact.txt", "a") as myfile:
+            myfile.write(post_input)
+        response = '<meta http-equiv="refresh" content="0; url=/cca/about-contact.html#thanks" />'
+    return [response.encode()]
+
---
+++ pre_git.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+
+# This script copies files into the git repo
+
+cp /home/jedmarum/app/*.* /home/jedmarum/app/cca/
+cp /home/jedmarum/app/templates/*.* /home/jedmarum/app/cca/templates/
+cp /home/jedmarum/contact/contact.py /home/jedmarum/app/cca/contact.py
